321289813682da1bc6406ba0563bc688a71cbb7a
travie09 <samuelayo09@gmail.com>
2021-02-05 18:43:42 +0100

    my javascript lesson

---
+++ lesson1.js
@@ -0,0 +1,30 @@
+// const a = 2;
+// const b = 3;
+// const c = 4;
+
+// const d = a+b+c;
+// console.log(d);
+
+// var m = 6;
+// var n = 3;
+// var y = m + n;
+// var g = y / 2;
+// console.log(g);
+
+var a = 2;
+var b = 6;
+var c = 4;
+
+var d = 2 * a;
+var e = b * b;
+var f = 4 * a * c;
+
+var g = e - f;
+var h = Math.sqrt(g);
+var i1 = -b + h;
+var i2 = -b - h;
+
+var x1 = i1 / d;
+var x2 = i2 / d;
+
+console.log(x1, x2);
